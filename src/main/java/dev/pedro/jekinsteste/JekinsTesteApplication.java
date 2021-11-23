2359f297e6434b52111fc4df1b2ecf58aa32789a
Pedro Brito <pedroisb28@gmail.com>
2021-11-23 09:33:48 -0300

    projeto criado

---
+++ src/main/java/dev/pedro/jekinsteste/JekinsTesteApplication.java
@@ -0,0 +1,13 @@
+package dev.pedro.jekinsteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekinsTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedro/jekinsteste/JekinsTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedro.jekinsteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
